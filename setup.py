272d6d1982917775c01bda7728cc30b61cb09bee
AmandaBirmingham <lists@imladris.com>
2022-10-26 08:52:54 -0700

    fixed missing comma

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'download_report_inputs=src.freyja_processing_utils:download_inputs'
+            'download_report_inputs=src.freyja_processing_utils:download_inputs',
             'make_freyja_metadata=src.generate_search_wastewater_lineages_report:generate_freyja_metadata',
             'make_search_reports=src.generate_search_wastewater_lineages_report:generate_reports',
             'make_campus_reports=src.generate_campus_wastewater_lineage_report:generate_reports',
