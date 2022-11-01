e982f6602ce67c33a20f43f3572830fa34535a37
AmandaBirmingham <lists@imladris.com>
2022-11-01 11:04:48 -0700

    Reformatted

--- scripts/update_freyja_job.sh
+++ scripts/update_freyja_job.sh
@@ -6,15 +6,17 @@
 # (FileNotFoundError: [Errno 2] No such file or directory: './lineagePaths.txt')
 # because an intermediate file can't be created.
 
-# Activate conda env freyja-env
 ANACONDADIR=/shared/workspace/software/anaconda3/bin
+
+# TODO: add some logging so we know if this failed!
+
+echo "updating freyja"
 source $ANACONDADIR/activate freyja-env
 
 # by default, the usher_barcodes.csv and curated_lineages.json files
 # end up in
 # /shared/workspace/software/anaconda3/envs/freyja-env/lib/python3.10/site-packages/freyja/data
+freyja update
 
-echo "updating freyja"
-# TODO: decide whether to put back the outdir param
-freyja update # --outdir $LOCAL_RUN_DIR
 source $ANACONDADIR/deactivate
+
